a618cc07dd558ea622ac6254ced052c2435f80ad
harvey mmangisa <harveymmangisa@gmail.com>
2025-08-10 20:06:02 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -5,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: "kwachalite",
-  appId: "1:470765994153:web:8222c38591986a13faa9b8",
-  storageBucket: "kwachalite.firebasestorage.app",
-  apiKey: "AIzaSyA5lSzpmwV8o4a14p9sxzd3jBhISj5HvIo",
-  authDomain: "kwachalite.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "470765994153"
+  "projectId": "kwachalite",
+  "appId": "1:470765994153:web:8222c38591986a13faa9b8",
+  "storageBucket": "kwachalite.firebasestorage.app",
+  "apiKey": "AIzaSyA5lSzpmwV8o4a14p9sxzd3jBhISj5HvIo",
+  "authDomain": "kwachalite.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "470765994153"
 };
 
 // Initialize Firebase
