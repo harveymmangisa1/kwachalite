a107cb326043fc0f855af8ae75bdcffc408f57c4
harveymmangisa1 <166815140+harveymmangisa1@users.noreply.github.com>
2025-09-18 01:13:44 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.tsx'
+import App from './App.tsx'
 import './app/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
